d477d54f70d294f9fedb69587ba7e685deca2e3d
Emil Haugberg <emilveen@gmail.com>
2018-07-28 17:53:14 +0200

    Clean up rotation code a bit.

--- src/Tetris/Rotate.purs
+++ src/Tetris/Rotate.purs
@@ -6,13 +6,6 @@ import Config
 import Tetris.Shape
 import Tetris.Rotation.Helper
 
-type Transformation =
-  { one   :: Block Number
-  , two   :: Block Number
-  , three :: Block Number
-  , four  :: Block Number
-  }
-
 type RotationPoint = Int
 data Rotation      = One | Two | Three | Four
 
@@ -21,47 +14,10 @@ derive instance eqRotation :: Eq Rotation
 rotation :: Shape -> Rotation -> Block Coordinate -> Block Coordinate
 rotation sh rot bc = blocksToCoord x y coordNum
   where
-    x = coords.x
-    y = coords.y
+    x        = coords.x
+    y        = coords.y
     coordNum = rotationToCoords rot sh
     coords   = f3 bc sh rot
-
-rotations :: Shape -> Transformation
-rotations sh = case sh of
-  Square -> {one: initSq  , two: initSq , three: initSq   , four: initSq }
-  Line   -> {one: initLine, two: sndLine, three: initLine , four: sndLine}
-  Z      -> {one: initZ, two: sndZ, three: initZ , four: sndZ}
-  S      -> {one: initS, two: sndS, three: initS , four: sndS}
-  L      -> {one: initL, two: sndL, three: thirdL , four: fourthL }
-  MirroredL -> {one: initMl, two: sndMl, three: thirdMl , four: fourthML }
-  T      -> {one: initT  , two: sndT , three: thirdT, four: fourthT }
-
-  where
-    initSq   = initialPos' Square
-
-    initLine = initialPos' Line
-    sndLine  = Block 0.0 1.0 2.0 3.0
-
-    initZ    = initialPos' Z
-    sndZ     = Block 0.0 1.0 (-1.0) (-2.0)
-
-    initS    = initialPos' S
-    sndS     = Block (-4.0) (-1.0) 1.0 2.0
-
-    initL    = initialPos' L
-    sndL     = Block 0.0 1.0 2.0 (-2.0)
-    thirdL   = Block 6.0 (2.0) (-1.0) (-2.0)
-    fourthL  = Block 4.0 2.0 1.0 0.0
-
-    initMl   = initialPos' MirroredL
-    sndMl    = Block (-4.0) 0.0 1.0 2.0
-    thirdMl  = Block (-1.0) 1.0 4.0 5.0
-    fourthML = Block 0.0 1.0 2.0 6.0
-
-    initT    = initialPos' T
-    sndT     = Block (-1.0) 0.0 1.0 2.0
-    thirdT   = Block (-1.0) 0.0 1.0 5.0
-    fourthT  = Block 0.0 1.0 2.0 5.0
 
 rotationToCoords :: Rotation -> Shape -> Block Number
 rotationToCoords rt sh = case rt of
@@ -82,7 +38,7 @@ nextRotation' Two   = Three
 nextRotation' Three = Four
 nextRotation' Four  = One
 
-f1 :: Shape -> SOM -> Z
+f1 :: Shape -> SOM -> RotationPoints
 f1 L y = y.l
 f1 Z y = y.z
 f1 T y = y.t
@@ -91,7 +47,7 @@ f1 MirroredL y = y.mirrl
 f1 Line y = y.line
 f1 Square y = y.square
 
-f2 :: Z -> Rotation -> { x :: Number, y :: Number }
+f2 :: RotationPoints -> Rotation -> { x :: Number, y :: Number }
 f2 z One = z.one
 f2 z Two = z.two
 f2 z Three = z.three
--- src/Tetris/Rotation/Helper.purs
+++ src/Tetris/Rotation/Helper.purs
@@ -4,7 +4,7 @@ import Config
 import Prelude
 import Tetris.Shape
 
-type Z =
+type RotationPoints =
   { one   :: {x :: Number,  y :: Number}
   , two   :: {x :: Number,  y :: Number}
   , three :: {x :: Number,  y :: Number}
@@ -12,22 +12,39 @@ type Z =
   }
 
 type SOM = {
-  l      :: Z,
-  z      :: Z,
-  t      :: Z,
-  s      :: Z,
-  mirrl  :: Z,
-  line   :: Z,
-  square :: Z
+  l      :: RotationPoints,
+  z      :: RotationPoints,
+  t      :: RotationPoints,
+  s      :: RotationPoints,
+  mirrl  :: RotationPoints,
+  line   :: RotationPoints,
+  square :: RotationPoints
 }
+
+type Transformation =
+  { one   :: Block Number
+  , two   :: Block Number
+  , three :: Block Number
+  , four  :: Block Number
+  }
 
 rotationCoords :: Block Coordinate -> SOM
 rotationCoords (Block a b c d) =
-  { l: {one: {x: b.x, y: a.y}, two: {x: b.x, y: b.y - blockHeight}, three: {x: b.x, y: b.y - blockHeight}, four: {x: b.x, y: c.y}}
-  , z: {one: {x: b.x, y: b.y}, two: {x: d.x, y: c.y - (blockHeight * 2.0)}, three: {x: c.x, y: b.y}, four: {x: b.x, y: c.y - (blockHeight * 2.0)}}
-  , t: {one: {x: b.x, y: b.y}, two: {x: b.x, y: b.y - blockHeight}, three: {x: b.x, y: c.y}, four: {x: b.x, y: b.y - blockHeight}}
-  , s: {one: {x: c.x, y: c.y}, two: {x: d.x, y: c.y - (blockHeight)}, three: {x: b.x, y: c.y}, four: {x: b.x, y: c.y - (blockHeight)}}
-  , mirrl: {one: {x: b.x, y: b.y}, two: {x: c.x, y: c.y - blockHeight}, three: {x: c.x, y: c.y}, four: {x: b.x, y: c.y - blockHeight}}
-  , line: {one: {x: c.x, y: c.y}, two: {x: c.x, y: c.y - (blockHeight * 2.0)}, three: {x: c.x - blockWidth, y: c.y}, four: {x: b.x, y: c.y - (blockHeight * 2.0)}}
-  , square: {one: {x: b.x, y: b.y}, two: {x: c.x, y: c.y}, three: {x: c.x - blockWidth, y: b.y - blockHeight}, four: {x: b.x, y: c.y}}
+  { l:      {one: {x: b.x, y: a.y}, two: {x: b.x, y: b.y - blockHeight        }, three: {x: b.x             , y: b.y - blockHeight}, four: {x: b.x, y: c.y                      }}
+  , z:      {one: {x: b.x, y: b.y}, two: {x: d.x, y: c.y - (blockHeight * 2.0)}, three: {x: c.x             , y: b.y              }, four: {x: b.x, y: c.y - (blockHeight * 2.0)}}
+  , t:      {one: {x: b.x, y: b.y}, two: {x: b.x, y: b.y - blockHeight        }, three: {x: b.x             , y: c.y              }, four: {x: b.x, y: b.y -  blockHeight       }}
+  , s:      {one: {x: c.x, y: c.y}, two: {x: d.x, y: c.y - (blockHeight)      }, three: {x: b.x             , y: c.y              }, four: {x: b.x, y: c.y -  blockHeight       }}
+  , mirrl:  {one: {x: b.x, y: b.y}, two: {x: c.x, y: c.y - blockHeight        }, three: {x: c.x             , y: c.y              }, four: {x: b.x, y: c.y -  blockHeight       }}
+  , line:   {one: {x: c.x, y: c.y}, two: {x: c.x, y: c.y - (blockHeight * 2.0)}, three: {x: c.x - blockWidth, y: c.y              }, four: {x: b.x, y: c.y - (blockHeight * 2.0)}}
+  , square: {one: {x: b.x, y: b.y}, two: {x: c.x, y: c.y                      }, three: {x: c.x - blockWidth, y: b.y - blockHeight}, four: {x: b.x, y: c.y                      }}
   }
+
+rotations :: Shape -> Transformation
+rotations sh = case sh of
+  Z         -> {one: Block (-4.0)   0.0  1.0 5.0, two: Block 0.0      1.0 (-1.0) (-2.0), three: Block (-4.0) 0.0   1.0    5.0 , four: Block   0.0    1.0 (-1.0) (-2.0)}
+  S         -> {one: Block (-1.0)   1.0  0.0 4.0, two: Block (-4.0) (-1.0) 1.0     2.0 , three: Block (-1.0) 1.0   0.0    4.0 , four: Block (-4.0) (-1.0)  1.0    2.0}
+  T         -> {one: Block (-4.0)   0.0  1.0 4.0, two: Block (-1.0)   0.0  1.0     2.0 , three: Block (-1.0) 0.0   1.0    5.0 , four: Block   0.0    1.0   2.0    5.0}
+  L         -> {one: Block (-4.0)   0.0  4.0 5.0, two: Block 0.0      1.0  2.0   (-2.0), three: Block   6.0  2.0 (-1.0) (-2.0), four: Block   4.0    2.0   1.0    0.0}
+  MirroredL -> {one: Block (-4.0) (-1.0) 0.0 4.0, two: Block (-4.0)   0.0  1.0     2.0 , three: Block (-1.0) 1.0   4.0    5.0 , four: Block   0.0    1.0   2.0    6.0}
+  Line      -> {one: Block (-4.0)   0.0  4.0 8.0, two: Block 0.0      1.0  2.0     3.0 , three: Block (-4.0) 0.0   4.0    8.0 , four: Block   0.0    1.0   2.0    3.0}
+  Square    -> {one: Block 0.0      1.0  4.0 5.0, two: Block 0.0      1.0  4.0     5.0 , three: Block   0.0  1.0   4.0    5.0 , four: Block   0.0    1.0   4.0    5.0}
